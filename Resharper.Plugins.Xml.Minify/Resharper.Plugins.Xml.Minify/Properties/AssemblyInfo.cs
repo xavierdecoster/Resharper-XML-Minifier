5d58a64565b7406b798e8baeeb49808aabe5b353
Xavier Decoster <xavier.decoster@gmail.com>
2012-02-08 19:40:00 +0100

    initial commit

---
+++ Resharper.Plugins.Xml.Minify/Resharper.Plugins.Xml.Minify/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+using System.Reflection;
+using JetBrains.Application.PluginSupport;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Resharper.Plugins.Xml.Minify")]
+[assembly: AssemblyDescription("This plugin adds a context action to XML files allowing you to minify the file.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Xavier Decoster")]
+[assembly: AssemblyProduct("Resharper.Plugins.Xml.Minify")]
+[assembly: AssemblyCopyright("Copyright © Xavier Decoster, 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// The following information is displayed by ReSharper in the Plugins dialog
+[assembly: PluginTitle("XML Minify Plugin")]
+[assembly: PluginDescription("This plugin adds a context action to XML files allowing you to minify the file.")]
+[assembly: PluginVendor("Xavier Decoster")]
